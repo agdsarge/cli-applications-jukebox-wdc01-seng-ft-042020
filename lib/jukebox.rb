7708f4ae65f8156d10886585659f788e14537edb
Andrew Sargeant <agdsargeant@gmail.com>
2020-04-11 04:34:54 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -18,7 +18,11 @@ def play(songs)
   puts "Please enter a song name or number:"
   reponse = gets.strip
   if songs.include?(response)
-    puts ""
+    puts "Playing #{response}"
+  elsif response.to_i > 0 and response.to_i < songs.length
+    puts "Playing #{songs[response.to_i - 1]}"
+  else
+    puts "Invalid input, please try again"
 end
 
 def exit_jukebox
